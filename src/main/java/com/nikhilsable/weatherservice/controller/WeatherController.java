b41ceca2aefdf7633a74ee635925987607a65762
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-28 15:12:11 +0530

    Implement WeatherController

---
+++ src/main/java/com/nikhilsable/weatherservice/controller/WeatherController.java
@@ -0,0 +1,38 @@
+package com.nikhilsable.weatherservice.controller;
+
+import com.nikhilsable.weatherservice.entity.Weather;
+import com.nikhilsable.weatherservice.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    // Get weather by city
+    @GetMapping
+    public String getWeather(@RequestParam String city) {
+        return weatherService.getWeatherByCity(city);
+    }
+
+    // Save weather
+    @PostMapping
+    public Weather addWeather(@RequestBody Weather weather) {
+        return weatherService.saveWeather(weather);
+    }
+
+    // Get all weather records
+    @GetMapping("/all")
+    public List<Weather> getAllWeather() {
+        return weatherService.getAllWeather();
+    }
+}
