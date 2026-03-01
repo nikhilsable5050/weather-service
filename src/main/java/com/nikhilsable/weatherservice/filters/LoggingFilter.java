210ab461a382152cb31b87fb553c52926da38778
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-01 16:12:54 +0530

    implement LoggingFilter for request URL logging

---
+++ src/main/java/com/nikhilsable/weatherservice/filters/LoggingFilter.java
@@ -0,0 +1,17 @@
+package com.nikhilsable.weatherservice.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        System.out.println("Request URL: "+ httpServletRequest.getRequestURL());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
