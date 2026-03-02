37616cf9d198b59b9bb8eb8da9d867c4dcf307d1
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-02 22:36:22 +0530

    feat(auth): implement CustomUserDetailsService

--- src/main/java/com/nikhilsable/weatherservice/service/CustomUserDetailsService.java
+++ src/main/java/com/nikhilsable/weatherservice/service/CustomUserDetailsService.java
@@ -1,4 +1,16 @@
 package com.nikhilsable.weatherservice.service;
 
+import com.nikhilsable.weatherservice.entity.Users;
+import com.nikhilsable.weatherservice.repository.UserDetailsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomUserDetailsService {
+    private final UserDetailsRepository userDetailsRepository;
+    public CustomUserDetailsService(UserDetailsRepository userDetailsRepository) {
+        this.userDetailsRepository = userDetailsRepository;
+    }
+    public Users saveUser(Users user) {
+        return userDetailsRepository.save(user);
+    }
 }
