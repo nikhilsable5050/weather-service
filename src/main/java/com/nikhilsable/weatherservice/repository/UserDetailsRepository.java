0fbc75695ac5778bc7abd545d8e490c101c75823
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-02 22:35:34 +0530

    feat(user): add UserDetailsRepository for user persistence

--- src/main/java/com/nikhilsable/weatherservice/repository/UserDetailsRepository.java
+++ src/main/java/com/nikhilsable/weatherservice/repository/UserDetailsRepository.java
@@ -1,4 +1,10 @@
 package com.nikhilsable.weatherservice.repository;
 
-public class UserDetailsRepository {
+import com.nikhilsable.weatherservice.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDetailsRepository extends JpaRepository<Users,Long> {
+    Optional<Users> findByUsername(String username);
 }
