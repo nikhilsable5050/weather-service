1bfaf05bd16bfbb742a35d29e319204608405ca4
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-02 22:35:47 +0530

    feat(user): add UserDetailsController for user registration

--- src/main/java/com/nikhilsable/weatherservice/controller/UserDetailsController.java
+++ src/main/java/com/nikhilsable/weatherservice/controller/UserDetailsController.java
@@ -1,4 +1,21 @@
 package com.nikhilsable.weatherservice.controller;
 
+import com.nikhilsable.weatherservice.entity.Users;
+import com.nikhilsable.weatherservice.service.CustomUserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserDetailsController {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public UserDetailsController(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @PostMapping
+    public Users createUser(@RequestBody Users user) {
+        return customUserDetailsService.saveUser(user);
+    }
 }
