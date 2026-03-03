87460a028043eb1cc262e35e51075b4493aadee5
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-03 17:29:08 +0530

    Update Users entity with unique username constraint and role field

--- src/main/java/com/nikhilsable/weatherservice/entity/Users.java
+++ src/main/java/com/nikhilsable/weatherservice/entity/Users.java
@@ -1,25 +1,34 @@
 package com.nikhilsable.weatherservice.entity;
 
 import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "users")
+@Entity                      // Marks this class as a JPA Entity
+@Table(name = "users")       // Table name in database
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Users {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    // Auto-increment primary key
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    // Username cannot be null and must be unique
     private String username;
+
+    @Column(nullable = false)
+    // Encoded password will be stored here
     private String password;
+
+    @Column(nullable = false)
+    // Role like USER or ADMIN
     private String role;
-
-
 }
